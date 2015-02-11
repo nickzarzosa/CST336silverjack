b2b3456030f8f0cb7cbd268f586b07ecc1763517
zarz2942 <nzarzosa@csumb.edu>
2015-02-11 19:53:20 +0000

    fix to index

--- index.php
+++ index.php
@@ -7,11 +7,13 @@ To change this template use Tools | Templates.
     <title>silverjack game</title>
 
         <link href="css/styles.css" rel="stylesheet"/>
-        <?php include 'functions.php';?>
+        <?php include 'includes/functions.php';?>
 
 
 </head>
 <body>
+    <h1><b>Silverjack</b></h1>
+    
     
 </body>
 </html>
