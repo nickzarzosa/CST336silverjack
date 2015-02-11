b34632806c927a01547527c4f721ead33ae61043
misarmiento <misarmiento@csumb.edu>
2015-02-11 19:14:18 +0000

    Changes to be committed: 	modified:   css/styles.css 	modified:   index.php

--- index.php
+++ index.php
@@ -5,8 +5,10 @@ To change this template use Tools | Templates.
 <html>
 <head>
     <title>silverjack game</title>
+        <link href="css/styles.css" rel="stylesheet"/>
 </head>
 <body>
-
+    <h1>Silver Jack Game</h1>
+    
 </body>
 </html>
