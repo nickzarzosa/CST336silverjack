a2192fa013856eefcd71b5146ddc45bab4162fe5
zarz2942 <nzarzosa@csumb.edu>
2015-02-13 04:56:06 +0000

    centered text with wrapper

--- includes/functions.php
+++ includes/functions.php
@@ -1,6 +1,4 @@
 <?php
 
-/*
- * To change this template use Tools | Templates.
- */
+
 ?>
--- index.php
+++ index.php
@@ -8,11 +8,18 @@ To change this template use Tools | Templates.
 
         <link href="css/styles.css" rel="stylesheet"/>
         <?php include 'includes/functions.php';?>
-
+        <meta charset="utf-8" />
+        
 
 </head>
 <body>
-    <h1><b>Silverjack</b></h1>
+    
+    <div id="wrapper">
+        <h1><b>Silverjack</b></h1>    
+        
+        
+        
+    </div>
     
     
 </body>
